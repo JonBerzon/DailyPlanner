02bc76021cca1fab3a564f9d941b84781d7401ed
Jonathan Berzon <jonathanlberzon@gmail.com>
2022-01-04 19:35:02 -0500

    User auth completed

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,41 @@
+class Api::UsersController < ApplicationController
+    
+    def index
+        @users = User.all 
+        render :index
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show 
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end 
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :bio, :fname, :lname)
+    end
+
+
+
+
+end
